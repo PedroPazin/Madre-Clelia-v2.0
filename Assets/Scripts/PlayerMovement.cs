ea59edf6ebaa54bbff613171c349cd21836d3afe
Pazin <pedrolucas.pazin@gmail.com>
2025-04-10 23:26:34 -0300

    feat: Básico da movimentação do player

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+public class PlaterMovement : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody2D _rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        _rb.linearVelocity = direction * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
