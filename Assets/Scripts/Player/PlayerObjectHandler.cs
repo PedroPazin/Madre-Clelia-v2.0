1422d184c2bc1a2b51df0c57780b5d4e0004a1d7
Pazin <pedrolucas.pazin@gmail.com>
2025-04-24 16:40:45 -0300

    feat: Personagem e Freira sprites

--- Assets/Scripts/Player/PlayerObjectHandler.cs
+++ Assets/Scripts/Player/PlayerObjectHandler.cs
@@ -6,7 +6,6 @@ using UnityEngine.InputSystem;
 
 public class PlayerObjectHandler : MonoBehaviour
 {
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
